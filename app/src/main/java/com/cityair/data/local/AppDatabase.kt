ea781567759915f1c29463ec845aff5ed9b274a0
Feathericed <iceb.featherings@gmail.com>
2026-08-13 20:35:00 -0400

    db connect success

---
+++ app/src/main/java/com/cityair/data/local/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.cityair.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(
+	entities = [CityEntity::class],
+	version = 1,
+	exportSchema =false
+)
+abstract class AppDatabase : RoomDatabase(){
+	abstract fun cityDao(): CityDao
+	companion object{
+		@Volatile
+		private var INSTANCE: AppDatabase? =null
+		
+		fun getDatabase(context: Context): AppDatabase{
+			return INSTANCE ?: synchronized(this){
+				val db = Room.databaseBuilder(
+					context.applicationContext,
+					AppDatabase::class.java,
+					"air_quality_db"
+				).build()
+				INSTANCE = db
+				db
+			}
+		}
+	
+	}
+
+}
